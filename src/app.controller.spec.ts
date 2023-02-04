f9bdeaf9441a8d070c210e15600a692afcf12ff2
F1rstID <wndhdks4536@gmail.com>
2023-02-04 14:20:01 +0900

    :bug: a
    d

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,3 +1,4 @@
+import { ConfigModule } from '@nestjs/config';
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -7,6 +8,7 @@ describe('AppController', () => {
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
+      imports: [ConfigModule.forRoot()],
       controllers: [AppController],
       providers: [AppService],
     }).compile();
@@ -15,10 +17,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe(
-        '키보드 심각하게 이상했는대 껏다키니까 바로 해결되어 버리는 이상한 상황이 일어나버렸지 뭐야',
-      );
+    it('return String', () => {
+      expect(appController.getHello()).toBe('hi');
     });
   });
 });
--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return '키보드 심각하게 이상했는대 껏다키니까 바로 해결되어 버리는 이상한 상황이 일어나버렸지 뭐야';
+    return process.env.TEST;
   }
 }
